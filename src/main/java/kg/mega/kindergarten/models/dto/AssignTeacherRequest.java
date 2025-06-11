3cdb4e6a629a500243f6bda8e1a43c2c535f86db
diller556 <adilhanmuratov2@gmail.com>
2025-06-12 03:51:26 +0600

    Method: Adding a teacher to a group

---
+++ src/main/java/kg/mega/kindergarten/models/dto/AssignTeacherRequest.java
@@ -0,0 +1,13 @@
+package kg.mega.kindergarten.models.dto;
+
+public class AssignTeacherRequest {
+    private Long teacherId;
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+}
